7702d7897c927abd123c2d102eaa484ec2a80960
fim4re <fimareyassine@gmail.com>
2023-07-18 15:14:51 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A C program that prints _putchar
+ * Return: always 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return(0);
+}
