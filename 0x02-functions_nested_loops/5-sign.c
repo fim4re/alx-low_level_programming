3ceb9439b08fa5f68b8df4c5ad4931265a0cbf80
fim4re <fimareyassine@gmail.com>
2023-07-18 18:23:12 -0700

    first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * Description: function print the signe of a number
+ * @n: is a number
+ * Return: if n > 0 return 1 if n = 0 return 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 48)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+}
