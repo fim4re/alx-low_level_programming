73bbc9075b10d3722edd0160adb5fd504bd32e00
fim4re <fimareyassine@gmail.com>
2023-07-18 20:34:58 -0700

    first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * add - add two intergers
+ * @a: first input
+ * @b: second input
+ * Return: result of a + b
+*/
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
