e9c53ed027779516d71549c077064f6a2524acd8
fim4re <fimareyassine@gmail.com>
2023-07-20 08:40:20 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - print a straight line
+ * @n: number of _ charater printed
+*/
+void print_line(int n)
+{
+	int chr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (chr = 1; chr <= n; chr++)
+			_putchar('_');
+		-putchar('\n');
+	}
+}
