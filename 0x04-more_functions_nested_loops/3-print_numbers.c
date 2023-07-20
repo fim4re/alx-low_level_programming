28b675d60cf4fb25d70a7535c5a9409491844f54
fim4re <fimareyassine@gmail.com>
2023-07-20 06:22:07 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ *
+ * Return: always 0
+*/
+void print_numbers(void)
+{
+	int n = 0;
+
+	for (n = 0, n <= 9, n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
