0c96ee4ac0031356af53b1748d335c457c2f294c
fim4re <fimareyassine@gmail.com>
2023-07-20 06:03:39 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+*/
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
