81cfa0f704f4dea9bf3038cda0b23029d618e960
fim4re <fimareyassine@gmail.com>
2023-07-20 07:09:35 -0700

    first commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,19 @@
 */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + 48);
+			k = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				k = j % 10;
+			}
+			_putchar(k + 48);
 		}
 	}
 	_putchar('\n');
