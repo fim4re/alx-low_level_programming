04330982b4a8c786f53f394fd01ab45c9d103d70
fim4re <fimareyassine@gmail.com>
2023-07-24 15:20:35 -0700

    first commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,13 @@
 */
 void print_rev(char *s)
 {
-	int i, n;
+	int i = 0;
 
-	n = 0;
-	while (s[n] != '\0')
-		n++;
 
-	for (i = n - 1; i >= 0; i--)
+	while (s[i])
+		i++;
+
+	while (i--)
 	{
 		_putchar(s[i]);
 	}
