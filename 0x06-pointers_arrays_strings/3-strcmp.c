106316bc42d817c013534c91777beb2f943209b9
fim4re <fimareyassine@gmail.com>
2023-07-27 18:01:55 -0700

    first commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,19 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int equal = 0;
-
-	while (*s1 != *s2)
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
-		if (*s1 == *s2)
-		{
-			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
-			break;
-		}
 		s1++;
 		s2++;
 	}
-	return (equal);
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
