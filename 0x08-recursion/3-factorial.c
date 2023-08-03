176914c7476c38d5812fce09a50e184ff05b9ab5
fim4re <fimareyassine@gmail.com>
2023-08-02 19:30:42 -0700

    first commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#iclude "main.h"
+/**
+ * factorial - fonds factorial
+ * @n: int
+ * return: int
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
