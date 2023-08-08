2542fe3cc5058e26f29ed92518e1fd42f50fb100
fim4re <fimareyassine@gmail.com>
2023-08-08 11:24:56 -0700

    first commit

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,11 +1,5 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Description: prints qoute
- *
- * Return: Always 0
-*/
+
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacke\"\n\t- Kevin Mitnick");
