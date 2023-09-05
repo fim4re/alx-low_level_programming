2f3703847581d360da3a65befff390dccee75884
fim4re <fimareyassine@gmail.com>
2023-09-04 19:19:38 -0700

    update commit for the task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * read_textfile - a function that reads a text file and prints it
@@ -12,17 +12,37 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f;
-	ssize_t bytes;
-	char buf[READ_BUF_SIZE * 8];
+	int file;
+	ssize_t read_check, wcount;
+	char *buffer;
 
-	if (!filename || !letters)
+	if (filename == NULL) /*check if file is present*/
 		return (0);
-	f = open(filename, O_RDONLY);
-	if (f == -1)
+
+	file = open(filename, O_RDONLY); /*open file*/
+
+	if (file == -1)
 		return (0);
-	bytes = read(f, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, &but[0], bytes);
-	close(f);
-	return (bytes);
+
+	/*get the size of buffer from number of letters*/
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	read_check = read(file, buffer, letters); /*read file*/
+	if (read_check == -1) /*check if read failed*/
+		return (0);
+
+	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
+	if (wcount == -1 || read_check != wcount) /*check if write failed*/
+		return (0);
+
+	free(buffer);
+
+	close(file); /*close file*/
+
+	return (wcount);
 }
