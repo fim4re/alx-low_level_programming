bf563360a164d67c1bb498fdc43ff4642155617d
fim4re <fimareyassine@gmail.com>
2023-11-23 18:29:33 -0800

    commit for the task

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * clear_bit - clear bit at index
+ * @nb: numbre
+ * @index: bit
+ *
+ * Return: 1 on successful, -1 on error
+*/
+int clear_bit(unsigned long int *nb, unsigned int index)
+{
+	if (index >= sizeof(nb) * 8)
+		return (-1);
+
+	if (*nb & 1L << index)
+		*nb ^= 1L << index;
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 unsigned long int _power(unsigned int base, unsigned int pow);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
+unsigned int flip_bits(unsigned long int nb, unsigned long int sc);
 unsigned int binary_to_uint(const char *bs);
 int get_endianness(void);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-void print_binary(unsigned long int n);
-int clear_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int nb, unsigned int index);
+int set_bit(unsigned long int *nb, unsigned int index);
+void print_binary(unsigned long int nb);
+int clear_bit(unsigned long int *nb, unsigned int index);
 int _putchar(char c);
 
 #endif
