d440ef8222187a1145ea4e251742bd1150b86dae
fim4re <fimareyassine@gmail.com>
2023-11-23 18:15:14 -0800

    commit for the task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+include "main.h"
+
+/**
+ * print_binary - print number as binary
+ * @nb: number
+ *
+ * Return: void
+*/
+void print_binary(unsigned long int nb)
+{
+	int bit = sizeof(nb) * 8, prt = 0;
+
+	while (bit)
+	{
+		if (nb & 1L << --bit)
+		{
+			_putchar('1');
+			prt++;
+		}
+		else if (prt)
+			_putchar('0');
+	}
+	if (!prt)
+		_putchar('0');
+}
