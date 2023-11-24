9c389db82a73379c05493fe680d72e288068ad7a
fim4re <fimareyassine@gmail.com>
2023-11-23 18:40:29 -0800

    commit for the task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+include "main.h"
+
+/**
+ * flip_bits - flip to get from nmbr to another
+ * @nb: first number
+ * @sc: second nbr
+ * Return: number to flip
+*/
+unsigned int flip_bits(unsigned long int nb, unsigned long int sc)
+{
+	unsigned long int value = nb ^ sc;
+	unsigned int cnt = 0;
+
+	while (value)
+	{
+		if (value & 1ul)
+			cnt++;
+		value = value >> 1;
+	}
+	return (cnt)
+
+}
