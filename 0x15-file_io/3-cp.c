efbcd94b681a84ee7460a4a0f98686beba9e0e0f
fim4re <fimareyassine@gmail.com>
2023-11-27 11:07:48 -0800

    commit for the task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,34 +10,34 @@
 
 /**
  * main - program that copies the content of a file to another
- * @ac: arg count
- * @av: arg vector
+ * @argc: arg count
+ * @argv: arg vector
  *
  * Return: 1 on success 0 on failure
 */
-int main(int ac, char **av)
+int main(int argc, char *argv[])
 {
 	int from_fd = 0, to_fd = 0;
 	ssize_t c;
 	char buf[READ_BUF_SIZE];
 
-	if (ac != 3)
+	if (argc != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from_fd = open(av[1], O_RDONLY);
+	from_fd = open(argv[1], O_RDONLY);
 
 	if (from_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 
 	if (to_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
 
 	while ((c = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 		if (write(to_fd, buf, c) != c)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
 
 	if (c == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
 
 	from_fd = close(from_fd);
 	to_fd = close(to_fd);
