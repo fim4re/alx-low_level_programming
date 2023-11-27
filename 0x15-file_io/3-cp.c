6c700d8685ae837f3a0b037dcbc604716a1b87f5
fim4re <fimareyassine@gmail.com>
2023-11-27 10:48:32 -0800

    commit for the task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,36 +17,36 @@
 */
 int main(int ac, char **av)
 {
-	int from_fd = 0, to_fd = 0;
-	ssize_t r;
+	int from = 0, to = 0;
+	ssize_t c;
 	char buf[READ_BUF_SIZE];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from_fd = open(av[1], O_RDONLY);
+	from = open(av[1], O_RDONLY);
 
-	if (from_fd == -1)
+	if (from == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 
-	if (to_fd == -1)
+	if (to == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	while ((r = read(from_fd, buf, READ_BUF_SIZE)) > 0)
-		if (write(to_fd, buf, r) != r)
+	while ((c = read(from, buf, READ_BUF_SIZE)) > 0)
+		if (write(to, buf, c) != c)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	if (r == -1)
+	if (c == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
-	from_fd = close(from_fd);
-	to_fd = close(to_fd);
+	from = close(from);
+	to = close(to);
 
-	if (from_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	if (from)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from), exit(100);
 
-	if (to_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	if (to)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, to), exit(100);
 
 	return (EXIT_SUCCESS);
 
