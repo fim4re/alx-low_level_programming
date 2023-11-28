0d7ec01209be1bef2ed8490605a9a467f64aeea7
fim4re <fimareyassine@gmail.com>
2023-11-27 18:13:33 -0800

    commit for the task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,13 +8,13 @@
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
@@ -117,11 +117,11 @@ void print_data(unsigned char *e_ident)
 void print_version(unsigned char *e_ident)
 {
 		printf("  Version:                           %d",
-			e_ident[EI_VERSION]);
+				e_ident[EI_VERSION]);
 
 		switch (e_ident[EI_VERSION])
 		{
-		case EV_CURRENT
+		case EV_CURRENT:
 			printf(" (current)\n");
 			break;
 		default:
