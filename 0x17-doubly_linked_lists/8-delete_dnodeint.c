9e46d1326dfa7979ef0cbcc6bfcb706eb2d14ae4
fim4re <fimareyassine@gmail.com>
2024-01-11 06:38:32 -0800

    commit for the task

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -31,7 +31,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	else
 	{
 		*head = node->next;
-		if (*head == NULL)
+		if (*head != NULL)
 			(*head)->prev = NULL;
 	}
 	free(node);
