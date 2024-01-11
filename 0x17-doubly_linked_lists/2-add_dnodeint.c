2c7ed0d51076ed307685a9fcfaca5b25c4c7bc07
fim4re <fimareyassine@gmail.com>
2024-01-11 04:21:48 -0800

    commit for the task

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,9 +17,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	new->next = *head;
 	new->n = n;
-	if (!*head)
-		new->next = NULL;
+	if (!*head){
+		*head = new;
+		new->next = NULL;}
 	else
+	{
+		new->next = *head;
 		(*head)->prev = new;
+		*head = new;
+	}
+
 	return (new);
 }
