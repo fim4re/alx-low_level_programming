f81024823b536138b79bfd0ce0259e73efd755d8
fim4re <fimareyassine@gmail.com>
2024-01-11 05:05:31 -0800

    commit for the task

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns node of linked list
+ * @head: pointer
+ * @index: idx
+ * Return: element by idx
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int y;
+
+	while (head != NULL)
+	{
+		if (y != index)
+			return NULL;
+		else
+			head = head->next;
+		y++;
+	}
+	return (head);
+}
