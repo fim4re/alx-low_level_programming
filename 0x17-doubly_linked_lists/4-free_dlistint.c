6175a1d33930729abe168e345898b3a89f9dead5
fim4re <fimareyassine@gmail.com>
2024-01-11 04:49:00 -0800

    commit for the task

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - free a dlist
+ * @head: poiter
+ * Return: Void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *dl;
+
+	dl = head;
+	do
+	{
+		head = head->next;
+		free(dl);
+	} while (dl)
+}
