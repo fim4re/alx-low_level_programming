ad4031d0884db35f81b42b3e1ad219d34a90c75b
fim4re <fimareyassine@gmail.com>
2024-02-01 10:43:30 -0800

    commit for the task

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Delete hash table.
+ * @hash_t: pointer a hash table.
+*/
+void hash_table_delete(hash_table_t *hash_t)
+{
+	hash_node_t *node_t, *node_tmp;
+	unsigned long int j;
+
+	for (j = 0; j < hash_t->size; j++)
+	{
+		if (hash_t->array[j] != NULL)
+		{
+			node_t = hash_t->array[j];
+			while (node_t != NULL)
+			{
+				node_tmp = node_t->next;
+				free(node_t->key);
+				free(node_t->value);
+				free(node_t);
+				node_t = node_tmp;
+			}
+		}
+	}
+	free(hash_t->array);
+	free(hash_t);
+}
